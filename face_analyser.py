a03aa5916ee299cf71b7381680b74e72825a1e89
megagernan <76407704+megagernan@users.noreply.github.com>
2023-09-07 23:26:25 +0200

    Add files via upload

--- face_analyser.py
+++ face_analyser.py
@@ -1,10 +1,36 @@
 import os
 import cv2
+import threading
 import numpy as np
 from tqdm import tqdm
-from utils import scale_bbox_from_center
+import concurrent.futures
+import default_paths as dp
+from dataclasses import dataclass
+from utils.arcface import ArcFace
+from utils.gender_age import GenderAge
+from utils.retinaface import RetinaFace
 
-detect_conditions = [
+cache = {}
+
+@dataclass
+class Face:
+    bbox: np.ndarray
+    kps: np.ndarray
+    det_score: float
+    embedding: np.ndarray
+    gender: int
+    age: int
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+    def __setitem__(self, key, value):
+        if hasattr(self, key):
+            setattr(self, key, value)
+        else:
+            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{key}'")
+
+single_face_detect_conditions = [
     "best detection",
     "left most",
     "right most",
@@ -15,28 +41,27 @@ detect_conditions = [
     "smallest",
 ]
 
-swap_options_list = [
-    "All Face",
-    "Specific Face",
-    "Age less than",
-    "Age greater than",
-    "All Male",
-    "All Female",
-    "Left Most",
-    "Right Most",
-    "Top Most",
-    "Bottom Most",
-    "Middle",
-    "Biggest",
-    "Smallest",
+multi_face_detect_conditions = [
+    "all face",
+    "specific face",
+    "age less than",
+    "age greater than",
+    "all male",
+    "all female"
 ]
+
+face_detect_conditions =  multi_face_detect_conditions + single_face_detect_conditions
+
 
 def get_single_face(faces, method="best detection"):
     total_faces = len(faces)
+
+    if total_faces == 0:
+        return None
+
     if total_faces == 1:
         return faces[0]
 
-    print(f"{total_faces} face detected. Using {method} face.")
     if method == "best detection":
         return sorted(faces, key=lambda face: face["det_score"])[-1]
     elif method == "left most":
@@ -56,139 +81,88 @@ def get_single_face(faces, method="best detection"):
     elif method == "smallest":
         return sorted(faces, key=lambda face: (face["bbox"][2] - face["bbox"][0]) * (face["bbox"][3] - face["bbox"][1]))[0]
 
-
-def analyse_face(image, model, return_single_face=True, detect_condition="best detection", scale=1.0):
-    faces = model.get(image)
-    if scale != 1: # landmark-scale
-        for i, face in enumerate(faces):
-            landmark = face['kps']
-            center = np.mean(landmark, axis=0)
-            landmark = center + (landmark - center) * scale
-            faces[i]['kps'] = landmark
-
-    if not return_single_face:
-        return faces
-
-    return get_single_face(faces, method=detect_condition)
-
+def filter_face_by_age(faces, age, method="age less than"):
+    if method == "age less than":
+        return [face for face in faces if face["age"] < age]
+    elif method == "age greater than":
+        return [face for face in faces if face["age"] > age]
+    elif method == "age equals to":
+        return [face for face in faces if face["age"] == age]
 
 def cosine_distance(a, b):
     a /= np.linalg.norm(a)
     b /= np.linalg.norm(b)
     return 1 - np.dot(a, b)
 
+def is_similar_face(face1, face2, threshold=0.6):
+    distance = cosine_distance(face1["embedding"], face2["embedding"])
+    return distance < threshold
 
-def get_analysed_data(face_analyser, image_sequence, source_data, swap_condition="All face", detect_condition="left most", scale=1.0):
-    if swap_condition != "Specific Face":
-        source_path, age = source_data
-        source_image = cv2.imread(source_path)
-        analysed_source = analyse_face(source_image, face_analyser, return_single_face=True, detect_condition=detect_condition, scale=scale)
-    else:
-        analysed_source_specifics = []
-        source_specifics, threshold = source_data
-        for source, specific in zip(*source_specifics):
-            if source is None or specific is None:
-                continue
-            analysed_source = analyse_face(source, face_analyser, return_single_face=True, detect_condition=detect_condition, scale=scale)
-            analysed_specific = analyse_face(specific, face_analyser, return_single_face=True, detect_condition=detect_condition, scale=scale)
-            analysed_source_specifics.append([analysed_source, analysed_specific])
 
-    analysed_target_list = []
-    analysed_source_list = []
-    whole_frame_eql_list = []
-    num_faces_per_frame = []
+class AnalyseFace:
+    def __init__(self, provider=["CPUExecutionProvider"], session_options=None):
+        self.detector = RetinaFace(model_file=dp.RETINAFACE_PATH, provider=provider, session_options=session_options)
+        self.recognizer = ArcFace(model_file=dp.ARCFACE_PATH, provider=provider, session_options=session_options)
+        self.gender_age = GenderAge(model_file=dp.GENDERAGE_PATH, provider=provider, session_options=session_options)
+        self.detect_condition = "best detection"
+        self.detection_size = (640, 640)
+        self.detection_threshold = 0.5
 
-    total_frames = len(image_sequence)
-    curr_idx = 0
-    for curr_idx, frame_path in tqdm(enumerate(image_sequence), total=total_frames, desc="Analysing face data"):
-        frame = cv2.imread(frame_path)
-        analysed_faces = analyse_face(frame, face_analyser, return_single_face=False, detect_condition=detect_condition, scale=scale)
+    def analyser(self, img, skip_task=[]):
+        bboxes, kpss = self.detector.detect(img, input_size=self.detection_size, det_thresh=self.detection_threshold)
+        faces = []
+        for i in range(bboxes.shape[0]):
+            feat, gender, age = None, None, None
+            bbox = bboxes[i, 0:4]
+            det_score = bboxes[i, 4]
+            kps = None
+            if kpss is not None:
+                kps = kpss[i]
+            if 'embedding' not in skip_task:
+                feat = self.recognizer.get(img, kpss[i])
+            if 'gender_age' not in skip_task:
+                gender, age = self.gender_age.predict(img, kpss[i])
+            face = Face(bbox=bbox, kps=kps, det_score=det_score, embedding=feat, gender=gender, age=age)
+            faces.append(face)
+        return faces
 
-        n_faces = 0
-        for analysed_face in analysed_faces:
-            if swap_condition == "All Face":
-                analysed_target_list.append(analysed_face)
-                analysed_source_list.append(analysed_source)
-                whole_frame_eql_list.append(frame_path)
-                n_faces += 1
-            elif swap_condition == "Age less than" and analysed_face["age"] < age:
-                analysed_target_list.append(analysed_face)
-                analysed_source_list.append(analysed_source)
-                whole_frame_eql_list.append(frame_path)
-                n_faces += 1
-            elif swap_condition == "Age greater than" and analysed_face["age"] > age:
-                analysed_target_list.append(analysed_face)
-                analysed_source_list.append(analysed_source)
-                whole_frame_eql_list.append(frame_path)
-                n_faces += 1
-            elif swap_condition == "All Male" and analysed_face["gender"] == 1:
-                analysed_target_list.append(analysed_face)
-                analysed_source_list.append(analysed_source)
-                whole_frame_eql_list.append(frame_path)
-                n_faces += 1
-            elif swap_condition == "All Female" and analysed_face["gender"] == 0:
-                analysed_target_list.append(analysed_face)
-                analysed_source_list.append(analysed_source)
-                whole_frame_eql_list.append(frame_path)
-                n_faces += 1
-            elif swap_condition == "Specific Face":
-                for analysed_source, analysed_specific in analysed_source_specifics:
-                    distance = cosine_distance(analysed_specific["embedding"], analysed_face["embedding"])
-                    if distance < threshold:
-                        analysed_target_list.append(analysed_face)
-                        analysed_source_list.append(analysed_source)
-                        whole_frame_eql_list.append(frame_path)
-                        n_faces += 1
+    def get_faces(self, image, scale=1., skip_task=[]):
+        if isinstance(image, str):
+            image = cv2.imread(image)
 
-        if swap_condition == "Left Most":
-            analysed_face = get_single_face(analysed_faces, method="left most")
-            analysed_target_list.append(analysed_face)
-            analysed_source_list.append(analysed_source)
-            whole_frame_eql_list.append(frame_path)
-            n_faces += 1
+        faces = self.analyser(image, skip_task=skip_task)
 
-        elif swap_condition == "Right Most":
-            analysed_face = get_single_face(analysed_faces, method="right most")
-            analysed_target_list.append(analysed_face)
-            analysed_source_list.append(analysed_source)
-            whole_frame_eql_list.append(frame_path)
-            n_faces += 1
+        if scale != 1: # landmark-scale
+            for i, face in enumerate(faces):
+                landmark = face['kps']
+                center = np.mean(landmark, axis=0)
+                landmark = center + (landmark - center) * scale
+                faces[i]['kps'] = landmark
 
-        elif swap_condition == "Top Most":
-            analysed_face = get_single_face(analysed_faces, method="top most")
-            analysed_target_list.append(analysed_face)
-            analysed_source_list.append(analysed_source)
-            whole_frame_eql_list.append(frame_path)
-            n_faces += 1
+        return faces
 
-        elif swap_condition == "Bottom Most":
-            analysed_face = get_single_face(analysed_faces, method="bottom most")
-            analysed_target_list.append(analysed_face)
-            analysed_source_list.append(analysed_source)
-            whole_frame_eql_list.append(frame_path)
-            n_faces += 1
+    def get_face(self, image, scale=1., skip_task=[]):
+        faces = self.get_faces(image, scale=scale, skip_task=skip_task)
+        return get_single_face(faces, method=self.detect_condition)
 
-        elif swap_condition == "Middle":
-            analysed_face = get_single_face(analysed_faces, method="middle")
-            analysed_target_list.append(analysed_face)
-            analysed_source_list.append(analysed_source)
-            whole_frame_eql_list.append(frame_path)
-            n_faces += 1
+    def get_averaged_face(self, images, method="mean"):
+        if not isinstance(images, list):
+            images = [images]
 
-        elif swap_condition == "Biggest":
-            analysed_face = get_single_face(analysed_faces, method="biggest")
-            analysed_target_list.append(analysed_face)
-            analysed_source_list.append(analysed_source)
-            whole_frame_eql_list.append(frame_path)
-            n_faces += 1
+        face = self.get_face(images[0], scale=1., skip_task=[])
 
-        elif swap_condition == "Smallest":
-            analysed_face = get_single_face(analysed_faces, method="smallest")
-            analysed_target_list.append(analysed_face)
-            analysed_source_list.append(analysed_source)
-            whole_frame_eql_list.append(frame_path)
-            n_faces += 1
+        if len(images) > 1:
+            embeddings = [face['embedding']]
 
-        num_faces_per_frame.append(n_faces)
+            for image in images[1:]:
+                face = self.get_face(image, scale=1., skip_task=[])
+                embeddings.append(face['embedding'])
 
-    return analysed_target_list, analysed_source_list, whole_frame_eql_list, num_faces_per_frame
+            if method == "mean":
+                avg_embedding = np.mean(embeddings, axis=0)
+            elif method == "median":
+                avg_embedding = np.median(embeddings, axis=0)
+
+            face['embedding'] = avg_embedding
+
+        return face
